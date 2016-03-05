4cd3d307ebd3f884aea67f87fbb9c9bbdc3ae3bb
dmcgree <dmcgree@css.edu>
2016-03-05 17:56:13 -0600

    Initial commit

---
+++ app/src/main/java/com/example/android/rssreader/MyListFragment.java
@@ -0,0 +1,62 @@
+//CIS 3334 Sec 700 Mobile Device Programming - Spring 2016
+//In Class 7 - Fragments
+//Date: 03/05/2016
+//Submitted by: Dana McGree
+
+package com.example.android.rssreader;
+
+/**
+ * Created by Dana on 3/5/2016.
+ */
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class MyListFragment extends Fragment {
+
+    private OnItemSelectedListener listener;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_rsslist_overview,
+                container, false);
+        Button button = (Button) view.findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateDetail("fake");
+            }
+        });
+        return view;
+    }
+
+    public interface OnItemSelectedListener {
+        public void onRssItemSelected(String link);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnItemSelectedListener) {
+            listener = (OnItemSelectedListener) context;
+        } else {
+            throw new ClassCastException(context.toString()
+                    + " must implement MyListFragment.OnItemSelectedListener");
+        }
+    }
+
+    // triggers update of the details fragment
+    public void updateDetail(String uri) {
+        // create fake data
+        String newTime = String.valueOf(System.currentTimeMillis());
+        // send data to activity
+        listener.onRssItemSelected(newTime);
+    }
+}
